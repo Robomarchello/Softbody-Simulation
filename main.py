0a7a29225057c352f57867e00401a10a4e8d5af7
RoboMarchello <faccclol123@gmail.com>
2023-01-19 10:23:55 +0200

    Project initialization

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.app import App
+
+if __name__ == '__main__':
+    App((1280, 720), 'Softbody simulation', 60).loop()
---
+++ src/app.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+class App():
+    def __init__(self, ScreenSize, caption, fps):
+        self.ScreenSize = ScreenSize
+        
+        self.screen = pygame.display.set_mode(ScreenSize)
+        pygame.display.set_caption(caption)
+
+        self.clock = pygame.time.Clock()
+        self.fps = fps
+
+    def loop(self):
+        screen = self.screen
+        while True:
+            self.clock.tick(self.fps)
+            screen.fill((255, 255, 255))
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    raise SystemExit
+            
+            pygame.display.update()
