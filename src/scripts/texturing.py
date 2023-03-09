1e47b0be2b367f7fe0cfc76dd06636d833d7ed78
RoboMarchello <faccclol123@gmail.com>
2023-03-09 21:49:24 +0200

    Why did I do this💀

--- src/scripts/texturing.py
+++ src/scripts/texturing.py
@@ -16,25 +16,25 @@ class App():
 
         self.texture = pygame.image.load('src/assets/texture.png').convert()
 
-        self.textureTri = [[0, 80], [80, 0], [150, 60]]
+        self.textureTri = [[0, 80], [80, 0], [160, 80]]
         self.mappedTri = [[300, 170], [330, 50], [410, 130]]
 
-        xPoses = numpy.take(self.textureTri, 0, axis=1)
-        yPoses = numpy.take(self.textureTri, 1, axis=1)
+        xPoses = numpy.take(self.mappedTri, 0, axis=1)
+        yPoses = numpy.take(self.mappedTri, 1, axis=1)
         self.textureRect = pygame.Rect(
             min(xPoses), min(yPoses), 
             max(xPoses) - min(xPoses), max(yPoses) - min(yPoses)
             )
 
-        self.TextureMap()
+        self.output = self.TextureMap()
 
         self.clock = pygame.time.Clock()
         self.fps = fps
 
     def TextureMap(self):
-        pixels = pygame.PixelArray(self.texture)
+        pixels = pygame.surfarray.array2d(self.texture)
 
-        xInterps = numpy.linspace(1.0, 0.0, self.textureRect.width)
+        xInterps = numpy.linspace(0.0, 1.0, self.textureRect.width)
         yInterps = numpy.linspace(0.0, 1.0, self.textureRect.height)
     
         sideL = numpy.subtract(self.textureTri[0], self.textureTri[1])
@@ -45,9 +45,21 @@ class App():
         sideRinterp = sideR * yPair + self.textureTri[1]
 
         segBetween = numpy.subtract(sideRinterp, sideLinterp)
+        allSegments = numpy.repeat(segBetween, xInterps.shape[0], axis=0)
         tileX = numpy.tile(xInterps, segBetween.shape[0])
-        xPair = numpy.repeat(tileX, 2).reshape(tileX.shape[0], 2)
+        tileXPair = numpy.repeat(tileX, 2).reshape(tileX.shape[0], 2)
+        
+        sideLAll = numpy.repeat(sideLinterp, xInterps.shape[0], axis=0)
 
+        TextureShape = (self.textureRect.height, self.textureRect.width)
+
+        positions = numpy.int_(numpy.multiply(allSegments, tileXPair) + sideLAll)
+        xPoses = numpy.take(positions, 0, axis=1) - 1
+        yPoses = numpy.take(positions, 1, axis=1) - 1
+        newPixels = numpy.array(pixels)[xPoses, yPoses]
+        newPixels = newPixels.reshape(TextureShape)
+        
+        return pygame.surfarray.make_surface(newPixels)
 
 
     def loop(self):
@@ -62,6 +74,9 @@ class App():
             pygame.draw.polygon(screen, (255, 0, 0), self.mappedTri, 2)
 
             pygame.draw.rect(screen, (255, 0, 0), self.textureRect, 2)
+
+            if self.output != None:
+                screen.blit(self.output, (0, 160))
 
             for event in pygame.event.get():
                 if event.type == QUIT:
